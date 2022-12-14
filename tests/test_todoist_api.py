412c92fd004a49a9ec29cd00141abe07a92ca86e
Gabor Schulz <gabor@gaborschulz.com>
2022-12-14 05:05:34 +0100

    Added more tests

--- tests/test_todoist_api.py
+++ tests/test_todoist_api.py
@@ -6,12 +6,15 @@ from dotenv import load_dotenv
 
 from pytodoist import TodoistAPI
 from pytodoist.exceptions import TodoistError
+from pytodoist.models import Task, Due
 
 load_dotenv()
 API_KEY = os.environ.get('TODOIST_API')
+INBOX_ID = ''
+TASK_ID = []
 
 
-@pytest.fixture
+@pytest.fixture()
 def todoist():
     todoist = TodoistAPI(api_key=API_KEY)
     return todoist
@@ -48,8 +51,11 @@ def test_get_project_by_pattern_unsynced(todoist):
 
 
 def test_get_project_by_pattern_synced_existing(todoist):
+    global INBOX_ID
     todoist.sync()
     project = todoist.get_project_by_pattern(pattern='Inbox')
+    print(project)
+    INBOX_ID = project.id
     assert project.name == 'Inbox'
 
 
@@ -62,3 +68,66 @@ def test_get_project_by_pattern_synced_notexisting(todoist):
 def test_get_stats(todoist):
     stats = todoist.get_stats()
     assert 'karma' in stats
+
+
+def test_add_task_no_due_date(todoist):
+    global TASK_ID
+    task = Task(content="Buy Raspberries", project_id=INBOX_ID)
+    assert task.temp_id
+    todoist.add_task(task)
+    todoist.commit()
+    print(task)
+    TASK_ID.append(task.id)
+    assert task.id
+
+
+def test_add_task_with_due_date(todoist):
+    global TASK_ID
+    task = Task(content="Buy Honey", project_id=INBOX_ID, due=Due(string='today'))
+    assert task.temp_id
+    todoist.add_task(task)
+    todoist.commit()
+    print(task)
+    TASK_ID.append(task.id)
+    assert task.id
+
+
+def test_get_task_str_id(todoist):
+    task = todoist.get_task(task_id=str(TASK_ID[0]))
+    assert task.id == str(TASK_ID[0])
+    assert task.content
+
+
+def test_get_task_int_id(todoist):
+    task = todoist.get_task(task_id=int(TASK_ID[0]))
+    assert task.id == str(TASK_ID[0])
+    assert task.content
+
+
+def test_close_task_nothing_provided(todoist):
+    with pytest.raises(TodoistError):
+        todoist.close_task()
+
+
+def test_close_task_by_model(todoist):
+    task_id = TASK_ID.pop()
+    task = todoist.get_task(task_id=task_id)
+    todoist.close_task(task=task)
+    todoist.commit()
+    task = todoist.get_task(task_id=task_id)
+    assert task.checked
+
+
+def test_close_task_by_id(todoist):
+    task_id = TASK_ID.pop()
+    todoist.close_task(task_id=task_id)
+    todoist.commit()
+    task = todoist.get_task(task_id=task_id)
+    assert task.checked
+
+
+def test_close_task_uncommitted(todoist):
+    task = Task(content='Buy Groceries', project_id=INBOX_ID)
+    todoist.add_task(task=task)
+    todoist.close_task(task=task)
+    todoist.commit()
