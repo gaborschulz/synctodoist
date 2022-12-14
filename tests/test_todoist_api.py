af266f85f68b204b04b67d8a6e0b12dd8c7c445d
Gabor Schulz <gabor@gaborschulz.com>
2022-12-14 07:47:29 +0100

    Refactored tests with more fixtures

--- tests/test_todoist_api.py
+++ tests/test_todoist_api.py
@@ -10,14 +10,32 @@ from pytodoist.models import Task, Due
 
 load_dotenv()
 API_KEY = os.environ.get('TODOIST_API')
-INBOX_ID = ''
-TASK_ID = []
 
 
 @pytest.fixture()
 def todoist():
     todoist = TodoistAPI(api_key=API_KEY)
     return todoist
+
+
+@pytest.fixture(scope='session')
+def synced_todoist():
+    todoist = TodoistAPI(api_key=API_KEY)
+    todoist.sync()
+    return todoist
+
+
+@pytest.fixture(scope='session')
+def project_inbox(synced_todoist):
+    return synced_todoist.get_project_by_pattern('Inbox')
+
+
+@pytest.fixture
+def task_added(synced_todoist, project_inbox):
+    task_added = Task(content="Buy Raspberries", project_id=project_inbox.id)
+    synced_todoist.add_task(task_added)
+    synced_todoist.commit()
+    return task_added
 
 
 def test_init(todoist):
@@ -50,18 +68,13 @@ def test_get_project_by_pattern_unsynced(todoist):
         todoist.get_project_by_pattern(pattern='Inbox')
 
 
-def test_get_project_by_pattern_synced_existing(todoist):
-    global INBOX_ID
-    todoist.sync()
-    project = todoist.get_project_by_pattern(pattern='Inbox')
-    print(project)
-    INBOX_ID = project.id
-    assert project.name == 'Inbox'
+def test_get_project_by_pattern_synced_existing(synced_todoist, project_inbox):
+    print(project_inbox)
+    assert project_inbox.name == 'Inbox'
 
 
-def test_get_project_by_pattern_synced_notexisting(todoist):
-    todoist.sync()
-    project = todoist.get_project_by_pattern(pattern='NON_EXISTING_PROJECT')
+def test_get_project_by_pattern_synced_notexisting(synced_todoist):
+    project = synced_todoist.get_project_by_pattern(pattern='NON_EXISTING_PROJECT')
     assert project is None
 
 
@@ -70,46 +83,33 @@ def test_get_stats(todoist):
     assert 'karma' in stats
 
 
-def test_add_task_no_due_date(todoist):
-    global TASK_ID
-    task = Task(content="Buy Raspberries", project_id=INBOX_ID)
+def test_add_task_no_due_date(synced_todoist, project_inbox):
+    task = Task(content="Buy Raspberries", project_id=project_inbox.id)
     assert task.temp_id
-    todoist.add_task(task)
-    todoist.commit()
+    synced_todoist.add_task(task)
+    synced_todoist.commit()
     print(task)
-    TASK_ID.append(task.id)
     assert task.id
 
 
-def test_add_task_with_due_date(todoist):
-    global TASK_ID
-    task = Task(content="Buy Honey", project_id=INBOX_ID, due=Due(string='today'))
+def test_add_task_with_due_date(synced_todoist, project_inbox):
+    task = Task(content="Buy Honey", project_id=project_inbox.id, due=Due(string='today'))
     assert task.temp_id
-    todoist.add_task(task)
-    todoist.commit()
+    synced_todoist.add_task(task)
+    synced_todoist.commit()
     print(task)
-    TASK_ID.append(task.id)
     assert task.id
 
 
-def test_get_task_str_id(todoist):
-    task = todoist.get_task(task_id=str(TASK_ID[0]))
-    assert task.id == str(TASK_ID[0])
+def test_get_task_str_id(synced_todoist, task_added):
+    task = synced_todoist.get_task(task_id=task_added.id)
+    assert task.id == task_added.id
     assert task.content
 
 
-def test_get_task_cached(todoist):
-    todoist.sync()
-    task_id = TASK_ID[0]
-    assert task_id in todoist.tasks
-    task = todoist.get_task(task_id=str(TASK_ID[0]))
-    assert task.id == str(TASK_ID[0])
-    assert task.content
-
-
-def test_get_task_int_id(todoist):
-    task = todoist.get_task(task_id=int(TASK_ID[0]))
-    assert task.id == str(TASK_ID[0])
+def test_get_task_int_id(synced_todoist, task_added):
+    task = synced_todoist.get_task(task_id=int(task_added.id))
+    assert task.id == task_added.id
     assert task.content
 
 
@@ -118,25 +118,25 @@ def test_close_task_nothing_provided(todoist):
         todoist.close_task()
 
 
-def test_close_task_by_model(todoist):
-    task_id = TASK_ID.pop()
-    task = todoist.get_task(task_id=task_id)
-    todoist.close_task(task=task)
-    todoist.commit()
-    task = todoist.get_task(task_id=task_id)
+def test_close_task_by_model(synced_todoist, task_added):
+    task = synced_todoist.get_task(task_id=task_added.id)
+    synced_todoist.close_task(task=task)
+    synced_todoist.commit()
+    task = synced_todoist.get_task(task_id=task_added.id)
     assert task.checked
 
 
-def test_close_task_by_id(todoist):
-    task_id = TASK_ID.pop()
-    todoist.close_task(task_id=task_id)
-    todoist.commit()
-    task = todoist.get_task(task_id=task_id)
+def test_close_task_by_id(synced_todoist, task_added):
+    synced_todoist.close_task(task_id=task_added.id)
+    synced_todoist.commit()
+    task = synced_todoist.get_task(task_id=task_added.id)
     assert task.checked
 
 
-def test_close_task_uncommitted(todoist):
-    task = Task(content='Buy Groceries', project_id=INBOX_ID)
-    todoist.add_task(task=task)
-    todoist.close_task(task=task)
-    todoist.commit()
+def test_close_task_uncommitted(synced_todoist, project_inbox):
+    task = Task(content='Buy Groceries', project_id=project_inbox.id)
+    synced_todoist.add_task(task=task)
+    synced_todoist.close_task(task=task)
+    synced_todoist.commit()
+    task = synced_todoist.get_task(task_id=task.id)
+    assert task.checked
