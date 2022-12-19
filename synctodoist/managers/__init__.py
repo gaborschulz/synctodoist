e65863caf3e163bf669600bb10ba8fed279c03b6
Gabor Schulz <gabor@gaborschulz.com>
2022-12-19 18:18:53 +0100

    synctodoist-28 Write documentation

--- synctodoist/managers/__init__.py
+++ synctodoist/managers/__init__.py
@@ -1,4 +1,10 @@
-"""This module provides access to various data models offered by Todoist."""
+"""
+This module provides access to various data managers for interacting with Todoist.
+
+There is a respective manager class for every Todoist data model of the package.
+
+These classes are never instantiated directly, instead, they are accessed through their accessors on the TodoistAPI class.
+"""
 
 from .label_manager import LabelManager
 from .project_manager import ProjectManager
