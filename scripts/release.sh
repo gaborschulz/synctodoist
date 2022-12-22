68ea16690a99638f2e46367938499b1feb5488ef
Gabor Schulz <gabor@gaborschulz.com>
2022-12-22 14:24:02 +0100

    synctodoist-28 Write documentation: completed tutorial

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,53 +1,62 @@
 #!/bin/zsh
 
-start=`date +%s`
+start=$(date +%s)
+VERSION_BUMPED=0
 
 load_env() {
-  if [ -f .env ]
-  then
-      echo "Loading .env file"
-      export $(cat .env | xargs)
-  fi
+    if [ -f .env ]; then
+        echo "Loading .env file"
+        export $(cat .env | xargs)
+    fi
 
-  echo "Project: $PROJECT_NAME"
+    echo "Project: $PROJECT_NAME"
 }
 
 run_test() {
-  pytest
+    pytest
 }
 
 cleanup() {
-  find . -type d -name "@eaDir" -print0 | xargs -0 rm -rf && \
-  rm -rf dist/*
+    find . -type d -name "@eaDir" -print0 | xargs -0 rm -rf
 }
 
 bump_version() {
-  echo "Argument: $1 / $#"
-  if [[ "$#" -eq 0 ]]; then
-    echo "No version bump"
-    return 0
-  fi
+    echo "Argument: $1 / $#"
+    if [[ "$#" -eq 0 ]]; then
+        echo "No version bump"
+        VERSION_BUMPED=0
+        return 0
+    fi
 
-  echo "Bumping version from $(poetry version)"
-  poetry version $1 && \
-  poetry version && \
-  git commit -am "Committing version $(poetry version --short)" && \
-  git tag -a v$(poetry version --short) -m "Tagging version $(poetry version --short)"
+    echo "Bumping version from $(poetry version)"
+    poetry version $1 &&
+        poetry version &&
+        git commit -am "Committing version $(poetry version --short)" &&
+        git tag -a v$(poetry version --short) -m "Tagging version $(poetry version --short)"
+    VERSION_BUMPED=1
 }
 
 push() {
-  git push origin v$(poetry version --short) && \
-  poetry build && \
-  aws s3 sync ./dist s3://gaborschulz.com/downloads/pytodoist
+    coverage-badge -o coverage.svg -f
+    git push origin v$(poetry version --short)
 }
 
-load_env && \
-cleanup && \
-run_test && \
-bump_version $1 && \
-push
+release_to_pypi() {
+    if [[ $VERSION_BUMPED == 0 ]]; then
+        echo "No PyPI release without version bump"
+    fi
 
-end=`date +%s`
-runtime=$((end-start))
+    poetry publish --build
+}
+
+load_env &&
+cleanup &&
+run_test &&
+# bump_version $1 &&
+push &&
+release_to_pypi
+
+end=$(date +%s)
+runtime=$((end - start))
 
 echo "Elapsed time: $runtime s"
