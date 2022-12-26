34bac0f197d9e59e94b3c5dfec3fcc90b33304a6
Gabor Schulz <gabor@gaborschulz.com>
2022-12-26 15:53:04 +0100

    synctodoist-37 Add custom version bump commit message

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,19 @@
 start=$(date +%s)
 VERSION_BUMPED=0
 
+RED="\033[1;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+RESET="\033[0m"
+
+QUESTION_FLAG="${GREEN}?"
+WARNING_FLAG="${YELLOW}!"
+NOTICE_FLAG="${CYAN}❯"
+
 load_env() {
     if [ -f .env ]; then
         echo "Loading .env file"
